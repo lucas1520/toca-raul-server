33d8a825e1043b54f4dfb53e75ea549c53d178f9
Lucas Lipinsky <lucaslip2005@gmail.com>
2026-05-17 01:29:49 -0300

    fix: corrige geracao de id automatica para tabela de usuarios

--- src/main/java/com/tocaraul/tocaraulserver/model/User.java
+++ src/main/java/com/tocaraul/tocaraulserver/model/User.java
@@ -1,6 +1,8 @@
 package com.tocaraul.tocaraulserver.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +12,8 @@ public class User {
     @Getter
     @Setter
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Getter
     @Setter
