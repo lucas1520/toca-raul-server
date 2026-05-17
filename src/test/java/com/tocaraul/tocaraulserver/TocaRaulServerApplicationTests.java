3a1827cfb558e8486aa4b20b2202870e0ceab834
Lucas Lipinsky <lucaslip2005@gmail.com>
2026-05-16 22:24:29 -0300

    initial commit

---
+++ src/test/java/com/tocaraul/tocaraulserver/TocaRaulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tocaraul.tocaraulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocaRaulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
