277090e27105b68fb98de3096ec8c44547c1523b
Lucas Lipinsky <lucaslip2005@gmail.com>
2026-05-24 14:43:56 -0300

    fix: adiciona relacao de artista com usuario

--- src/main/java/com/tocaraul/tocaraulserver/model/Artist.java
+++ src/main/java/com/tocaraul/tocaraulserver/model/Artist.java
@@ -1,9 +1,12 @@
 package com.tocaraul.tocaraulserver.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -41,5 +44,11 @@ public class Artist {
     @Getter
     @Setter
     private String startPrice;
+
+    @Getter
+    @Setter
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", unique = true, nullable = false)
+    private User user;
 
 }
