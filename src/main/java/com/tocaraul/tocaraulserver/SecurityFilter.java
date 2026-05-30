6299c9785d488f21b7ab5abb2f3fc541d393c0be
Lucas Lipinsky <lucaslip2005@gmail.com>
2026-05-30 00:16:52 -0300

    refactor: melhora a legibilidade do codigo

--- src/main/java/com/tocaraul/tocaraulserver/SecurityFilter.java
+++ src/main/java/com/tocaraul/tocaraulserver/SecurityFilter.java
@@ -26,23 +26,29 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
+        String token = this.recoverToken(request);
+
         if (token != null) {
-            var email = tokenService.validateToken(token);
+            String email = tokenService.validateToken(token);
 
             if (!email.isEmpty()) {
                 UserDetails user = userRepository.findByEmail(email);
 
-                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
+
         filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request) {
-        var authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) return null;
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return null;
+        }
+
         return authHeader.replace("Bearer ", "");
     }
 }
